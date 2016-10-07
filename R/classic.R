637e3f1af70b49aa44f38cfde5b0541c2a00616d
Cheng <YU19864@roswellpark.org>
2016-10-07 16:49:14 -0400

    add input_classic() function

---
+++ R/classic.R
@@ -0,0 +1,19 @@
+#' Classic R approaches
+#'
+#' @details There are two files, from a microarray experiment. ALL-phenoData.csv
+#' describes the 128 samples, ALL-expression.csv are the normalized expressions
+#' values for 12k probesets across 128 samples.
+#'
+#' @param pdata_file
+#'
+#' @param exprs_file
+#'
+#' @return A data.frame() containing samples as rows and phenotype data and
+#' expression data as columns.
+#'
+#' @export
+input_classic <- function(pdata_file, exprs_file) {
+    pdata <- read.csv(pdata_file, row.names = 1, check.names = FALSE)
+    exprs <- read.csv(exprs_file, row.names = 1, check.names = FALSE)
+    merge(pdata, t(exprs), by = 0)
+}
